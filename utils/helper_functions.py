532b9afc48650b321dd9a813be1cc11b4af4414b
Falk Amelung <famelung@earth.miami.edu>
2023-12-30 23:58:34 -0500

    Revert "added requirements.txt"
    This reverts commit 016ac5a962cdea29fcbf39913a394e61b9f44f0e.

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -5,6 +5,7 @@ import subprocess
 import glob
 from mintpy.utils import readfile, writefile
 from mintpy.objects import HDFEOS
+from mintpy.utils.arg_utils import create_argument_parser
 import numpy as np
 from pathlib import Path
 
@@ -17,9 +18,8 @@ EXAMPLE = """example:
 def create_parser(subparsers=None):
     synopsis = 'Plotting of InSAR, GPS and Seismicity data'
     epilog = EXAMPLE
-    # parser = create_argument_parser(name, synopsis=synopsis, description=synopsis, epilog=epilog, subparsers=subparsers)
-    parser = argparse.ArgumentParser(description=synopsis, epilog=epilog)
-    
+    name = __name__.split('.')[-1]
+    parser = create_argument_parser(name, synopsis=synopsis, description=synopsis, epilog=epilog, subparsers=subparsers)
     line_file = os.getenv('RSMASINSAR_HOME') + '/tools/plotdata' + '/data/hawaii_lines_new.mat'
 
     parser.add_argument('data_dir', nargs='*', help='Directory(s) with InSAR data.\n')
